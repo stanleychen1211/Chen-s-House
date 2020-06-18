4d0595efb653623c87ca09201d353e809a0faaed
Stanley Chen <stanley61603@gmail.com>
2020-06-17 19:47:03 -0700

    Chen's House

---
+++ src/shared/dishes.js
@@ -0,0 +1,43 @@
+export const DISHES =
+    [
+        {
+        id: 0,
+        name:'Minced pork rice',
+        image: '/assets/images/minceporkrice.jpg',
+        category: 'mains',
+        label:'Hot',
+        price:'5.99',
+        featured: true,
+        description:'Minced pork rice, ground pork marinated and boiled in soy sauce served on top of steamed rice and egg.'                    
+        },
+        {
+        id: 1,
+        name:'Beef Noodle',
+        image: '/assets/images/beef noodle.jpg',
+        category: 'mains',
+        label:'',
+        price:'9.99',
+        featured: false,
+        description:'Beef noodle soup made of stewed or red braised beef, beef broth, vegetables and Chinese noodles.'
+        },
+        {
+        id: 2,
+        name:'Stinky Tofu',
+        image: '/assets/images/stinky tofu.jpg',
+        category: 'appetizer',
+        label:'New',
+        price:'4.99',
+        featured: false,
+        description:'Stinky tofu, made by soaking fresh tofu in a brine of fermented milk, meat or vegetables.'
+        },
+        {
+        id: 3,
+        name:'Taro ball with brown suger ice',
+        image: '/assets/images/dessert.jpeg',
+        category: 'dessert',
+        label:'',
+        price:'4.99',
+        featured: false,
+        description:'Shaved ice, red beans, sweet potato, pearls, taro balls with jelly.'
+        }
+    ];
